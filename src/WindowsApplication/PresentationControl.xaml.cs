693f710412c33dfb6b1ec9bc8aa8cd20114912ac
Erez Amihud <erezamihud@gmail.com>
2022-07-09 23:31:06 +0300

    start the transformation

--- src/WindowsApplication/PresentationControl.xaml.cs
+++ src/WindowsApplication/PresentationControl.xaml.cs
@@ -33,70 +33,26 @@ namespace WindowsApplication
          * StartChat
          */
 
-        public int Size
+
+
+
+
+        public object[] Items
         {
-            get { return (int)GetValue(SizeProperty); }
-            set { SetValue(SizeProperty, value); }
+            get { return (object[])GetValue(ItemsProperty); }
+            set { SetValue(ItemsProperty, value); }
         }
 
-        // Using a DependencyProperty as the backing store for Size.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty SizeProperty =
-            DependencyProperty.Register("Size", typeof(int), typeof(PresentationControl));
-
-
-
-        public object Item
-        {
-            get { return (object)GetValue(ItemProperty); }
-            set { SetValue(ItemProperty, value); }
-        }
-
-        // Using a DependencyProperty as the backing store for Item.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty ItemProperty =
-            DependencyProperty.Register("Item", typeof(object), typeof(PresentationControl), new PropertyMetadata(0));
-
+        // Using a DependencyProperty as the backing store for Items.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ItemsProperty =
+            DependencyProperty.Register("Items", typeof(object[]), typeof(PresentationControl));
 
 
         public PresentationControl()
         {
             InitializeComponent();
-            DataContext = this;
+            DataContext = new PresentationViewModel(Items);
         }
 
-        public static readonly RoutedEvent DecreaseIndexEvent = EventManager.RegisterRoutedEvent(
-            "StartChat",
-            RoutingStrategy.Bubble,
-            typeof(RoutedEventHandler),
-            typeof(PresentationControl)
-        );
-
-        public event RoutedEventHandler DecreaseIndex
-        {
-            add { AddHandler(DecreaseIndexEvent, value); }
-            remove { RemoveHandler(DecreaseIndexEvent, value); }
-        }
-
-        public static readonly RoutedEvent IncreaseIndexEvent = EventManager.RegisterRoutedEvent(
-            "IncreaseIndex",
-            RoutingStrategy.Bubble,
-            typeof(RoutedEventHandler),
-            typeof(PresentationControl)
-        );
-
-        public event RoutedEventHandler IncreaseIndex
-        {
-            add { AddHandler(IncreaseIndexEvent, value); }
-            remove { RemoveHandler(IncreaseIndexEvent, value); }
-        }
-
-        private void IncreaseIndexClick(object sender, RoutedEventArgs e)
-        {
-            RaiseEvent(new RoutedEventArgs(IncreaseIndexEvent));
-        }
-
-        private void DecreaseIndexClick(object sender, RoutedEventArgs e)
-        {
-            RaiseEvent(new RoutedEventArgs(DecreaseIndexEvent));
-        }
-    }
+       
 }
---
+++ src/WindowsApplication/ViewModules/PresentationViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WindowsApplication.Utilities;
+
+namespace WindowsApplication.ViewModules
+{
+    class PresentationViewModel : ObservableCollection<object>
+    {
+        private int _index;
+        public int Index { get => _index; set {
+                _index = value;
+                OnPropertyChanged(new System.ComponentModel.PropertyChangedEventArgs("Index"));
+                OnPropertyChanged(new System.ComponentModel.PropertyChangedEventArgs("SelectedItem"));
+            } 
+        }
+
+        public object SelectedItem
+        {
+            get => this[_index];
+        }
+    }
+}
