44f2e07a76d275d8dec46aeae995194e73de098f
Erez Amihud <erezamihud@gmail.com>
2022-07-14 22:31:29 +0300

    fix api

--- src/WindowsApplication/ViewModules/ChatViewModel.cs
+++ src/WindowsApplication/ViewModules/ChatViewModel.cs
@@ -28,12 +28,28 @@ namespace WindowsApplication.ViewModules
 
             // , () => newMessageText != ""
             SendMessage = new RelayCommand(sendMessageFromBox, () => newMessageText != "");
+            var dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+            EventHandler handler = async (sender, e) => {
+                try
+                {
+                    object getMessages = await App.ServiceProvider.GetService<IJustaSessionService>().JustaApi.
+                    ListSessionMessagesMessageGetAsync(ChatID, skip: Messages.Count);
+
+                    onNewMessages(getMessages);
+                } catch(Exception ee)
+                {
+                    // this code will be deleted shortly
+                }
+            };
+            dispatcherTimer.Tick += handler;
+            dispatcherTimer.Interval = TimeSpan.FromSeconds(1);
+            dispatcherTimer.Start();
             //SendMessage = new RelayCommand(sendMessageFromBox);
 
 
 
-            Task<object> getMessages = App.ServiceProvider.GetService<IJustaSessionService>().JustaApi.ListMessagesMessageSessionIdGetAsync(ChatID);
-            getMessages.ContinueWith((f) => onNewMessages(f.Result));
+            /*Task<object> getMessages = App.ServiceProvider.GetService<IJustaSessionService>().JustaApi.ListSessionMessagesMessageGetAsync(ChatID);
+            getMessages.ContinueWith((f) => onNewMessages(f.Result));*/
             // App.ServiceProvider.GetService<IJustaSessionService>().JustaApi.
         }
 
@@ -43,23 +59,23 @@ namespace WindowsApplication.ViewModules
             JToken b;
             arr.ToList().ForEach(x => {
                 JObject obj = x.ToObject<JObject>();
-                Messages.Add(new ChatItemViewModel(false, (string)obj.GetValue("content")));
+                Messages.Add(new ChatItemViewModel((string)obj.GetValue("expert_id") == "", (string)obj.GetValue("content")));
                 }
             );
 
         }
 
-        private async void sendMessageFromBox()
+        private void sendMessageFromBox()
         {
             // TODO This message object should be the datacontext to every message in the list
-            App.ServiceProvider.GetService<IJustaSessionService>().JustaApi.CreateMessageMessagePost(new Client.Model.Message(ChatID, "me", newMessageText));
+            App.ServiceProvider.GetService<IJustaSessionService>().JustaApi.CreateMessageMessagePost(new Client.Model.Message(ChatID, "", newMessageText));
             newMessageText = "";
             OnPropertyChanged("newMessageText");
-
+            /*
             object getMessages = await App.ServiceProvider.GetService<IJustaSessionService>().JustaApi.
-                ListMessagesMessageSessionIdGetAsync(ChatID, skip:Messages.Count);
+                ListSessionMessagesMessageGetAsync(ChatID, skip:Messages.Count);
             
-            onNewMessages(getMessages);
+            onNewMessages(getMessages);*/
             
         }
 
